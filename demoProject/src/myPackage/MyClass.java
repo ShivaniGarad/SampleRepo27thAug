d88867c5ce6e2dc408fa912bf9aadf8771e999c6
ShivaniGarad <123741973+ShivaniGarad@users.noreply.github.com>
2023-01-28 16:16:10 +0530

    Basic Task

---
+++ demoProject/src/myPackage/MyClass.java
@@ -0,0 +1,15 @@
+package myPackage;
+
+public class MyClass {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("TestBase");
+		System.out.println("Pom.xml");
+		System.out.println("Utility");
+		System.out.println("Suite");
+		System.out.println("Resources");
+		
+	}
+
+}
